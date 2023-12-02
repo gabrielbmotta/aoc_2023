a7627dc2ab379fd631221f5c5972ee1e0cc4fa7e
Gabriel B. Motta <gabrielbenmotta@gmail.com>
2023-12-02 02:52:32 -0500

    Day 2 part 2

--- day_02/gbm_zig/src/main.zig
+++ day_02/gbm_zig/src/main.zig
@@ -12,11 +12,9 @@ fn game_value(source_str: []const u8, num_red: i32, num_green: i32, num_blue: i3
 
     for (source_str[5..]) |ch| {
         if (ch == ':') break;
-        try stdout.print("{c}", .{ch});
         game_ind = game_ind * dec_mult + try std.fmt.charToDigit(ch, 10);
     }
 
-    try stdout.print("   ", .{});
     var map = std.AutoHashMap(u8, i32).init(allocator);
     try map.put('d', num_red);
     try map.put('n', num_green);
@@ -35,22 +33,40 @@ fn game_value(source_str: []const u8, num_red: i32, num_green: i32, num_blue: i3
                 amount = amount * dec_mult + try std.fmt.charToDigit(ch, 10);
             }
 
-            // try stdout.print(" {s} ", .{entry});
-            try stdout.print("{any} ", .{amount});
-            try stdout.print("/{}/ ", .{entry[entry.len - 1]});
-            try stdout.print("[{}] ", .{map.get(entry[entry.len - 1]) orelse 0});
-
             if (amount > map.get(entry[entry.len - 1]) orelse map.get(entry[entry.len - 1]) orelse std.math.maxInt(i32)) {
-                // try stdout.print("Game {}: ", .{game_ind});
-                // try stdout.print("amt higher {} ", .{amount});
-                // try stdout.print("map val {} ", .{map.get(entry[entry.len - 1]) orelse 0});
                 return 0;
             }
         }
     }
-
-    try stdout.print(" !!! {} !!! ", .{game_ind});
     return game_ind;
+}
+
+fn game_power(source_str: []const u8) !i32 {
+    const col_ind: usize = std.mem.indexOf(u8, source_str, ":") orelse return 0;
+    var games = std.mem.tokenizeSequence(u8, source_str[col_ind + 1 ..], ";");
+
+    var map = std.AutoHashMap(u8, i32).init(allocator);
+    try map.put('d', 0);
+    try map.put('n', 0);
+    try map.put('e', 0);
+
+    const dec_mult: i32 = 10;
+
+    while (games.next()) |game| {
+        var entries = std.mem.tokenizeSequence(u8, game, ",");
+        while (entries.next()) |entry| {
+            var amount: i32 = 0;
+            for (entry[1..]) |ch| {
+                if (ch == ' ') break;
+                amount = amount * dec_mult + try std.fmt.charToDigit(ch, 10);
+            }
+
+            if (amount > map.get(entry[entry.len - 1]) orelse map.get(entry[entry.len - 1]) orelse std.math.maxInt(i32)) {
+                try map.put(entry[entry.len - 1], amount);
+            }
+        }
+    }
+    return map.get('d').? * map.get('n').? * map.get('e').?;
 }
 
 pub fn main() !void {
@@ -62,9 +78,12 @@ pub fn main() !void {
     const file_data = try file.reader().readAllAlloc(allocator, stat.size);
     var lines = std.mem.tokenizeSequence(u8, file_data, "\n");
 
-    var sum: i32 = 0;
+    var val_sum: i32 = 0;
+    var power_sum: i32 = 0;
     while (lines.next()) |line| {
-        sum += try game_value(line, 12, 13, 14);
-        try stdout.print(" = {}\n", .{sum});
+        val_sum += try game_value(line, 12, 13, 14);
+        power_sum += try game_power(line);
     }
+    try stdout.print("Part 1 = {}\n", .{val_sum});
+    try stdout.print("Part 2 = {}\n", .{power_sum});
 }
