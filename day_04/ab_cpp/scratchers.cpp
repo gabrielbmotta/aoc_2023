ef382521d82dbe243a8966dc1da3891418029825
Andrew Blankley <andrew.blankley@proton.me>
2023-12-08 21:42:22 -0500

    Update for part 2

--- day_04/ab_cpp/scratchers.cpp
+++ day_04/ab_cpp/scratchers.cpp
@@ -16,12 +16,36 @@ int main(int argc, char **argv)
 
     std::fstream input(argv[1]);
     std::string card;
-    auto points = 0;
+    // auto points = 0;
+    std::vector<int> number_of_cards;
+    auto game_number = 0;
     while (std::getline(input, card))
     {
-        points += PlayScratchers(card);
+        if (number_of_cards.size() == game_number)
+        {
+            number_of_cards.push_back(1);
+        }
+
+        auto cards = PlayScratchers(card);
+        while (number_of_cards.size() < game_number + cards + 1)
+        {
+            number_of_cards.push_back(1);
+        }
+
+        for (auto i = game_number + 1; i < game_number + cards + 1; i++)
+        {
+            number_of_cards[i] += number_of_cards[game_number];
+        }
+
+        game_number++;
     }
-    std::cout << "Total points: " << points << std::endl;
+
+    auto sum = 0;
+    for (auto it = number_of_cards.begin(); it != number_of_cards.end(); it++)
+    {
+        sum += *it;
+    }
+    std::cout << "Total cards: " << sum << std::endl;
 }
 
 int PlayScratchers(const std::string &card)
@@ -49,7 +73,8 @@ int PlayScratchers(const std::string &card)
     }
 
     // Now look at what we have
-    auto points = 0;
+    // auto points = 0;
+    auto cards = 0;
     while (std::getline(buf, str, ' '))
     {
         if (str.empty())
@@ -63,17 +88,19 @@ int PlayScratchers(const std::string &card)
         {
             if (number == *it)
             {
-                if (points == 0)
-                {
-                    points = 1;
-                }
-                else
-                {
-                    points *= 2;
-                }
+                cards++;
+                // if (points == 0)
+                // {
+                //     points = 1;
+                // }
+                // else
+                // {
+                //     points *= 2;
+                // }
             }
         }
     }
 
-    return points;
+    // return points;
+    return cards;
 }
