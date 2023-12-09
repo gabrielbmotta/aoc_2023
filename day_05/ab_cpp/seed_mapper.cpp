170e00017ce6babcf0f92be985cd2e4374b0bacf
Andrew Blankley <andrew.blankley@proton.me>
2023-12-09 07:32:13 -0500

    Initialize project source

---
+++ day_05/ab_cpp/seed_mapper.cpp
@@ -0,0 +1,9 @@
+#include <seed_mapper.h>
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "SeedMapper v" << SeedMapper_VERSION_MAJOR << "."
+              << SeedMapper_VERSION_MINOR << std::endl;
+}
