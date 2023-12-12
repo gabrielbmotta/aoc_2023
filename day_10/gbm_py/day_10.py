79708d2d333d69e4a8c810930338f2092c941e7e
Gabriel B Motta <gbmotta@mgh.harvard.edu>
2023-12-12 18:47:30 -0500

    Day 10

--- day_10/gbm_py/day_10.py
+++ day_10/gbm_py/day_10.py
@@ -13,12 +13,43 @@ def load_map(file):
         return np.array([list(line.strip('\n')) for line in input])
 
 
-def find_start(map):
-    for i in range(len(map)):
-        for j in range(len(map[i])):
-            if map[i][j] == "S":
+def find_start(path_map):
+    for i in range(len(path_map)):
+        for j in range(len(path_map[i])):
+            if path_map[i][j] == "S":
                 return i, j
 
+
+def setup_map_path(path_map, start_point):
+    path = list()
+    path.append(start_point)
+    potential_connections = list()
+    rel_dir = list()
+
+    if path_map[start_point[0] - 1][start_point[1]] in ('|', 'F', '7'):
+        potential_connections.append((start_point[0] - 1, start_point[1]))
+        rel_dir.append('N')
+
+    if path_map[start_point[0] + 1][start_point[1]] in ('|', 'J', 'L'):
+        potential_connections.append((start_point[0] + 1, start_point[1]))
+        rel_dir.append('S')
+
+    if path_map[start_point[0]][start_point[1] - 1] in ('-', 'L', 'F'):
+        potential_connections.append((start_point[0], start_point[1] - 1))
+        rel_dir.append('W')
+
+    if path_map[start_point[0]][start_point[1] + 1] in ('-', 'J', '7'):
+        potential_connections.append((start_point[0], start_point[1] + 1))
+        rel_dir.append('E')
+
+    for key, value in dir_dict.items():
+        if set(value) == set(rel_dir):
+            start_equivalent = key
+
+    path.insert(0, potential_connections[0])
+    path.append(potential_connections[-1])
+
+    return path, start_equivalent
 
 def find_new_adj(path_map, path_points):
     point = path_points[-1]
@@ -39,30 +70,29 @@ def find_new_adj(path_map, path_points):
 
 
 path_map = load_map("input.txt")
-start = find_start(path_map)
-print(f'{start[0]}, {start[1]}')
-path_points = list()
-path_points.append(start)
-potential_connections = list()
+start_point = find_start(path_map)
+path, start_eq = setup_map_path(path_map, start_point)
 
-if path_map[start[0] - 1][start[1]] in ('|', 'F', '7'):
-    potential_connections.append((start[0] - 1, start[1]))
-
-if path_map[start[0] + 1][start[1]] in ('|', 'J', 'L'):
-    potential_connections.append((start[0] + 1, start[1]))
-
-if path_map[start[0]][start[1] - 1] in ('-', 'L', 'F'):
-    potential_connections.append((start[0], start[1] - 1))
-
-if path_map[start[0]][start[1] + 1] in ('-', 'J', '7'):
-    potential_connections.append((start[0], start[1] + 1))
-
-print(potential_connections)
-
-path_points.insert(0, potential_connections[0])
-path_points.append(potential_connections[-1])
-
-while find_new_adj(path_map, path_points):
+while find_new_adj(path_map, path):
     continue
 
-print(f'Part 1: {len(path_points) / 2}')
+print(f'Part 1: {len(path) / 2}')
+
+map[start_point[0]][start_point[1]] = start_eq
+
+# OF-----7O
+# O|IIF-7|O
+# OL--JOLJO
+
+inside_count = 0
+for i, row in enumerate(path_map):
+    inside = False
+    for j, _ in enumerate(row):
+        if (i, j) in path:
+            if path_map[i][j] in ('|', 'L', 'J'):  # alternativley (| F 7)
+                inside = not inside
+        else:
+            inside_count += 1 if inside else 0
+
+print(f'Part 2: {inside_count}')
+
